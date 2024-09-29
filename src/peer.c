cd0763cdd91df168b9ee7059404ac7402dfde36b
ajschofield <git@ajschof.me>
2024-09-29 16:37:34 +0100

    add peer.c and header for initial connection listening

---
+++ src/peer.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include "socket.h"
+
+// Set a max size for the messages so that we don't overflow the buffer.
+#define MAX_BUFFER 1024
+
+void listen_for_connections(int sockfd)
+{
+    struct sockaddr_in client_addr;
+    int addrlen = sizeof(client_addr);
+    char buffer[MAX_BUFFER] = {0};
+
+    // Listen for any incoming connections.
+    if (listen(sockfd, 3) != 0)
+    {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+}
