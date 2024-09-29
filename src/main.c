4ee04869d8ec6102d953a0db0a0a9139dd64ffe8
ajschofield <git@ajschof.me>
2024-09-29 16:29:44 +0100

    add main.c as primary executable

---
+++ src/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "socket.h"
+
+int main(void)
+{
+    printf("Starting...\n");
+
+    int sockfd = create_socket();
+    if (sockfd < 0)
+    {
+        fprintf(stderr, "Socket creation failed.");
+        exit(EXIT_FAILURE);
+    }
+}
