0180ea9cf2440884d3fc6990199199b82ed48016
ajschofield <git@ajschof.me>
2024-09-30 00:34:33 +0100

    update main.c to establish connection with peer

--- src/main.c
+++ src/main.c
@@ -10,24 +10,24 @@
 
 int main(void)
 {
-    // printf("Starting...\n");
-
-    // int sockfd = create_socket();
-    // if (sockfd < 0)
-    // {
-    //     fprintf(stderr, "Socket creation failed.");
-    //     exit(EXIT_FAILURE);
-    // }
-
-    // listen_for_broadcasts();
-
-    // send_broadcast();
-
-    // listen_for_connections(sockfd);
-
     struct sockaddr_in peer_addr = discover_peer();
 
     printf("Peer found at %s:%d\n", inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+
+    int connection_fd = establish_connection(peer_addr);
+    if (connection_fd < 0)
+    {
+        fprintf(stderr, "Failed to establish connection with peer\n");
+        return -1;
+    }
+
+    printf("Connection established!\n");
+
+    // TESTING: Keep alive for ten seconds - to test under different network conditions
+    sleep(10);
+
+    close(connection_fd);
+    printf("Connection closed\n");
 
     return 0;
 }
